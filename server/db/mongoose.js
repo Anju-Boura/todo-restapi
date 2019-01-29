b18d20eda2b504cce5897f5b89bc95b90a0e64f8
Anju-Boura <anjuboura.2017@gmail.com>
2019-01-29 16:40:37 +0530

    mlab settings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose =require('mongoose')
+var {db}=require('./../../credentials')
 mongoose.Promise=global.Promise
 
-mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true })
+mongoose.connect(db,{ useNewUrlParser: true })
 
 module.exports = {
     mongoose :mongoose
