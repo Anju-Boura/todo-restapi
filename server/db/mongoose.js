789b1039a147cbcd54fe9f6660e3082c2db826fb
Anju-Boura <anjuboura.2017@gmail.com>
2019-01-31 14:51:10 -0600

    changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose =require('mongoose')
-//var {db}=require('./../../credentials')
+var {db}=require('./../../credentials')
 mongoose.Promise=global.Promise
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true })
+mongoose.connect(db || 'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true })
 
 module.exports = {
     mongoose :mongoose
